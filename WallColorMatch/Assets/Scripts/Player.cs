4a9bd85a92ad0de0e8b0812e2fdcaafcd876a173
jungheol <jungheol@naver.com>
2024-01-09 15:47:16 +0900

    마우스를 클릭할 때마다 플레이어가 점프하도록 구현

--- WallColorMatch/Assets/Scripts/Player.cs
+++ WallColorMatch/Assets/Scripts/Player.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Player : MonoBehaviour {
+    
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rigid;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
     }
 }
