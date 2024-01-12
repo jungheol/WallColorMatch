e36dedad9cf9cecf41e79c54047401c6bf7617e0
jungheol <jungheol@naver.com>
2024-01-12 16:22:03 +0900

    게임 시작 전 Player가 움직이고, 시작 후에 점프 기능 되도록 구현.

--- WallColorMatch/Assets/Scripts/Player.cs
+++ WallColorMatch/Assets/Scripts/Player.cs
@@ -10,22 +10,51 @@ public class Player : MonoBehaviour {
     public GameObject dieParticle;
 
     private Rigidbody2D rigid;
+    private CircleCollider2D coll;
     private SpriteRenderer spriteRenderer;
+    private bool isClick = false;
 
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        rigid.isKinematic = true;
+        coll = GetComponent<CircleCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        rigid.velocity = new Vector2(moveSpeed, jumpForce);
     }
 
-    private void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            Jump();
+    private IEnumerator Start() {
+        float originY = transform.position.y;
+        float delayY = 0.5f;
+        float moveSpeedY = 5;
+    
+        while (!isClick) {
+            float y = originY + delayY * Mathf.Sin(Time.time * moveSpeedY);
+            transform.position = new Vector2(transform.position.x, y);
+            
+            yield return null;
+        }
+    }
+
+    public void GameStart() {
+        rigid.isKinematic = false;
+        rigid.velocity = new Vector2(moveSpeed, jumpForce);
+        isClick = true;
+
+        StartCoroutine(UpdateInput());
+    }
+
+    IEnumerator UpdateInput() {
+        while (true) {
+            if (Input.GetMouseButtonDown(0)) {
+                Jump();
+            }
+            
+            yield return null;
         }
     }
 
     private void Jump() {
         rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
+        Debug.Log("jump");
     }
 
     private void ReverseX() {
